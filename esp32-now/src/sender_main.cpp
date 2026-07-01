89e4b74f7848ee3747e3204a396e58811e45abe2
Sara Atluri <sara@atluri.org>
2026-07-01 15:54:52 -0400

    minor cleanup

--- esp32-now/src/sender_main.cpp
+++ esp32-now/src/sender_main.cpp
@@ -4,6 +4,7 @@
   Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files.
   The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
 *********/
+#include <Arduino.h>
 #include <esp_now.h>
 #include <WiFi.h>
 
